7f5debf2a9573122df8851146960b7a761c982e4
Musika Iqfat <musfikaiqfatmomo21.com>
2025-09-27 14:52:51 +0600

    solved Data type problem

---
+++ Data-types.js
@@ -0,0 +1,11 @@
+const i = 4;
+const d = 4.0;
+const s = "HackerRank ";
+
+function performOperation(secondInteger, secondDecimal, secondString) {
+    console.log(i + (+secondInteger));
+    
+    console.log(d + (+secondDecimal));
+    
+    console.log(s + secondString);
+}
